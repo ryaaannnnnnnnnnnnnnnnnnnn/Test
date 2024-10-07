dc9a9830f367c334087b70fb8e626ae60cf32773
Ryan <126rgranada@frhsd.com>
2024-10-07 14:02:28 -0400

    first commit

---
+++ js/review.js
@@ -0,0 +1,20 @@
+// 1.   Declare a variable named number and assign the value 100 to it.
+let number = 100;
+// 2.   Add 10 to the value of number and store the result back in number.
+number = number + 10;
+// 3.   Add another 10 to the value of number using the += operator.
+number +=10;
+// 4.   Log the current value of number to the console.
+console.log(number);
+// 5.   Declare a variable named age and assign the value 7 to it.
+let age = 7;
+// 6.   Create a string that says "Tommy is" followed by the value of age and "years old."
+"Tommy is " + age + " years old.";
+// 7.   Create the same string using template literals.
+ageStringTemplate = 'Tommy is ${age} years old.';
+// 8.   Declare a constant named currency and assign the value "$" to it.
+const currency = '$';
+// 9.   Declare a variable named userIncome and assign the value 85000 to it.
+let userIncome = 85000;
+// 10.  Create a string that says "Your income is" followed by the currency and userIncome, and log it to the console.
+console.log(string3);
